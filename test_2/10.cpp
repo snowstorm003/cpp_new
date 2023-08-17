42c9f90c421e0ebbc42d0749c2a659be7ef748d7
snowstorm003 <tusharmukhopadhyay03@gmail.com>
2023-08-18 04:24:44 +0530

    folder renamed

---
+++ test_2/10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void remove_duplicate(int arr[], int size)
+{
+
+}
+
+int main()
+{
+    int arr[] = {}
+}
---
+++ test_2/15.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+string merge(string trv1[], string trv2[], string cit[], int size)
+{
+    for(int i=0; i<size; i++)
+    {
+        
+    }
+}
