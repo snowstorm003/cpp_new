305073e90f3f7a4b191fb155a932dedd4ba51b35
snowstorm003 <tusharmukhopadhyay03@gmail.com>
2023-10-09 23:43:41 +0530

    remove duplicates from an array

---
+++ neetcode/Arrays & Hashing/remove_duplicate.cpp
@@ -0,0 +1,17 @@
+//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        for(int i=0; i<nums.size()-1; i++)
+            if(nums[i]==nums[i+1])
+                return true;
+
+        return false;
+    }
+};
