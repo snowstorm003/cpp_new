0564aef6615d97c1fcea83bc1966f2f524718ce5
snowstorm003 <tusharmukhopadhyay03@gmail.com>
2023-10-26 21:58:09 +0530

    array reverse

---
+++ pujo_30_prgs/14.cpp
@@ -0,0 +1,38 @@
+//14. Write a program to reverse an array.
+
+#include <iostream>
+using namespace std;
+
+void reverse(int arr[], int arr2[], int size)
+{
+    int j=0;
+    for(int i=size-1; i>=0; i--)        
+    {
+        arr2[i]=arr[j];
+        j++;
+    }
+
+    for(int i=0; i<size; i++)       //better approach
+        arr2[i]=arr[size-i-1];
+}
+
+int main()
+{
+    int arr[] = {10, 56, 999, -9, 0, -9999, 48};
+
+    int size=sizeof(arr)/sizeof(arr[0]);
+
+    int arr2[size];
+
+    reverse(arr, arr2, size);
+
+    cout<<" Old Array:-";
+    for(int i=0; i<size; i++)
+        cout<<" "<<arr[i];
+
+    cout<<"\n New Array:-";
+    for(int i=0; i<size; i++)
+        cout<<" "<<arr2[i];
+    
+    return 0;
+}
