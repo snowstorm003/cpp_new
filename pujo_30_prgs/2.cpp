ad7f6adb07d634d116cb42bba677148ba80ff733
snowstorm003 <tusharmukhopadhyay03@gmail.com>
2023-10-26 19:36:35 +0530

    odd or even

---
+++ pujo_30_prgs/2.cpp
@@ -0,0 +1,17 @@
+//Write a program to check if a given number is even or odd.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin>>num;
+
+    if(num%2==0)
+        cout<<"Even";
+    else
+        cout<<"False";
+    
+    return 0;
+}
