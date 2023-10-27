2448a5f8e3c89ac49d8989420b182d2b0a1a548b
snowstorm003 <tusharmukhopadhyay03@gmail.com>
2023-10-28 01:24:03 +0530

    remove duplciates using map/set

--- pujo_30_prgs/13.cpp
+++ pujo_30_prgs/13.cpp
@@ -1,33 +1,61 @@
 //13. Write a C++ program to remove duplicate elements from an array.(sorted array)
 
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int remove_duplicates(int arr[], int len)
-{
-    int i=0;
+// void remove_duplicates(int arr[], int len)
+// {
+//     unordered_map<int, bool> check;
+//     for(int i=0; i<len; i++)
+//     {
+//         if(check.find(arr[i])==check.end())
+//             cout<<arr[i]<<" ";
+//         check[arr[i]]=false;
+//     }
 
-    for(int j=1; j<len; j++)
+// }
+
+void remove_duplicates(int arr[], int len)
+{
+    unordered_set<int> check;
+    for(int i=0; i<len; i++)
     {
-        if(arr[j]!=arr[i])
+        if(check.find(arr[i])==check.end())
         {
-            i++;
-            arr[i]=arr[j];
+            check.insert(arr[i]);
+            cout<<arr[i]<<" ";
         }
     }
-
-    return (i+1);
 }
+
+
+// two pointer approach for sorted arrays
+// int remove_duplicates(int arr[], int len)
+// {
+//     int i=0;
+
+//     for(int j=1; j<len; j++)
+//     {
+//         if(arr[j]!=arr[i])
+//         {
+//             i++;
+//             arr[i]=arr[j];
+//         }
+//     }
+
+//     return (i+1);
+// }
 
 int main()
 {
-    int arr[] = {1, 1, 2, 2, 2, 3, 3, 33}; //sorted array
+    int arr[] = {6, 9, 2, 15, 0, 4, 6, 2, 0, 99, 99}; 
     int len = sizeof(arr)/sizeof(arr[0]);
 
-    int pos = remove_duplicates(arr, len);
+    remove_duplicates(arr, len);
 
-    for(int i=0; i<pos; i++)
-        cout<<arr[i]<<" ";
+    // for(int i=0; i<pos; i++)
+    //     cout<<arr[i]<<" ";
 
     return 0;
 }
